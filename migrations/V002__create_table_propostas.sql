e2b320c2d8858430d97d02c405dcbcb52f0e44d1
josehelioaraujo <helioandrade@hotmail.com>
2026-08-29 20:58:44 -0300

    feat: add SQL migrations  V001 to V004

---
+++ migrations/V002__create_table_propostas.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE IF NOT EXISTS proposta.propostas (
+    id              UUID          PRIMARY KEY,
+    nome_cliente    VARCHAR(200)  NOT NULL,
+    cpf             VARCHAR(20)   NOT NULL,
+    tipo_seguro     INT           NOT NULL,
+    valor           DECIMAL(10,2) NOT NULL,
+    status          INT           NOT NULL DEFAULT 1,
+    criado_em       TIMESTAMP     NOT NULL DEFAULT NOW(),
+    atualizado_em   TIMESTAMP     NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_propostas_cpf_tipo
+    ON proposta.propostas (cpf, tipo_seguro);
---
+++ migrations/V004__create_table_contratacoes.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE IF NOT EXISTS contratacao.contratacoes (
+    id               UUID         PRIMARY KEY,
+    proposta_id      UUID         NOT NULL,
+    cpf              VARCHAR(20)  NOT NULL,
+    data_contratacao TIMESTAMP    NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX IF NOT EXISTS idx_contratacoes_proposta_id
+    ON contratacao.contratacoes (proposta_id);
