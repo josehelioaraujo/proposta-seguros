ff75fa6687cd3eef7062cba6cf13f234e5f4c6b7
josehelioaraujo <helioandrade@hotmail.com>
2026-08-29 21:23:42 -0300

    fix: add V005 migration  add criado_em column to contratacoes table

---
+++ migrations/V005__add_criado_em_contratacoes.sql
@@ -0,0 +1,6 @@
+﻿-- ============================================================
+-- V005__add_criado_em_contratacoes.sql
+-- Adiciona coluna criado_em na tabela contratacoes
+-- ============================================================
+ALTER TABLE contratacao.contratacoes
+    ADD COLUMN IF NOT EXISTS criado_em TIMESTAMP NOT NULL DEFAULT NOW();
